147f0388d1382d8615a59d750e383e0f5e5571d8
Sixto <sixto@acemail.com>
2023-07-24 16:31:19 -0300

    ponele

---
+++ scrypt.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector("[data-chiste-btn]");
+
+JokeAPI.getJokes({
+    jokeType: "single"
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      updateUI(data);
+      console.log(data)
+    });
+  
+ 
+    function updateUI(jokeData) {
+    const $ = (id) => document.getElementById(id);
+  
+    $("chiste").innerHTML = jokeData.joke;
+    
+};
+  
+btn.addEventListener("click", () => { JokeAPI.getJokes({
+    jokeType: "single"
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      updateUI(data);
+    });
+
+})
